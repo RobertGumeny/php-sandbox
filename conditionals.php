9e2b57bef57d60135169fc81d7ac9aadfecfdc37
Robert Gumeny <robert.gumeny528@gmail.com>
2020-08-23 11:50:01 -0600

    conditionals

---
+++ conditionals.php
@@ -0,0 +1,26 @@
+<?php 
+    # NOTE Conditionals
+
+    /*
+        ==
+        ===
+        <
+        >
+        <=
+        >=
+        !=
+        !==
+    */
+
+    $num = 5;
+
+    if($num == 5) {
+      echo '5 has passed';
+      echo '<br>';
+    }
+
+    if ($num === 5) {
+      echo '5 matches correct type';
+    }
+
+?>
