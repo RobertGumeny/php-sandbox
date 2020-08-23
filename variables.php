3873a220a4a99d84eb45a57ed786b0b73371669b
Robert Gumeny <robert.gumeny528@gmail.com>
2020-08-22 23:44:51 -0600

    variables and string concatenation

--- variables.php
+++ variables.php
@@ -1,3 +1,49 @@
 <?php
-    echo 'Hello World!';
+// Single line comment
+
+# Single Line Comment
+
+/*
+    Multi-line comment
+*/
+
+# NOTE VARIABLES
+/*
+  - Prefix with a $
+  - Must start with a letter or an underscore
+  - Only letters, numbers, and underscores
+  - Case sensitive ($Output $= $output)
+*/
+
+    $output = 'Hello World!';
+    echo $output;
+    echo '<br>';
+
+# NOTE DATA TYPES
+/*
+    String - "" or ''
+    Integers
+    Floats
+    Booleans
+    Arrays
+    Objects
+    Null
+    Resource
+*/
+
+    $num1 = 4;
+    $num2 = 10;
+    $sum = $num1 + $num2;
+
+    $string1 = 'My name is ';
+    $string2 = 'Robert';
+    $greeting = $string1 . $string2; // Concaetnate with a . instead of a +
+    
+
+    $float1 = 4.4;
+    $bool = true;
+
+    echo $sum;
+    echo "<br>";
+    echo $greeting;
 ?>
